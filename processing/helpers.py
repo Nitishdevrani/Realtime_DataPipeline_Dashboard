1d79724674f7b361b89ceb110824c6d6e05c8f54
moritzrengert <moritz@rengert.de>
2025-01-24 15:51:29 +0100

    partial data loading for testing

--- processing/helpers.py
+++ processing/helpers.py
@@ -2,15 +2,28 @@
 Some helper functions for the processing module.
 """
 
+from typing import Generator
 import pandas as pd
+from pyarrow.parquet import ParquetFile
+import pyarrow as pa
 
 
-def load_data(file_path):
+def load_data(file_path: str, n: int) -> pd.DataFrame:
     """Load parquet file."""
+    if n:
+        rows = next(ParquetFile(file_path).iter_batches(batch_size=n))
+        return pa.Table.from_batches([rows]).to_pandas()
     return pd.read_parquet(file_path)
 
 
-def get_rows(df):
+def get_rows(df: pd.DataFrame) -> Generator[pd.Series, None, None]:
     """Get rows of a dataframe one by one."""
     for _, row in df.iterrows():
         yield row
+
+
+def upload_data(processed_data: pd.DataFrame) -> None:
+    """Upload data to the dashboard."""
+    # TODO: Implement this function with real upload logic.
+    for processed_row in get_rows(processed_data):
+        print(processed_row.keys())
--- processing/processing.py
+++ processing/processing.py
@@ -1,9 +1,13 @@
 """
 Load the cleaned data, process it and send to the dashboard.
 """
-from helpers import load_data, get_rows
+
+from helpers import load_data, upload_data
+
 
 if __name__ == "__main__":
-    df = load_data("data/serverless/serverless_full.parquet")
-    for row in get_rows(df):
-        print(row)
+    df = load_data("data/serverless/serverless_full.parquet", n=10)
+
+    # TODO: Process the data
+
+    upload_data(df)
