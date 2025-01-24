1f828823f94521e13bd8118241f8f698965a3dc2
moritzrengert <moritz@rengert.de>
2025-01-24 23:07:56 +0100

    add state manager, make upload and backups async, restructure processing to prepare for use in pipeline

--- processing/helpers.py
+++ processing/helpers.py
@@ -2,6 +2,7 @@
 Some helper functions for the processing module.
 """
 
+import asyncio
 import time
 from typing import Generator
 import pandas as pd
@@ -9,7 +10,7 @@ from pyarrow.parquet import ParquetFile
 import pyarrow as pa
 
 
-def load_data(file_path: str, n: int) -> pd.DataFrame:
+def load_data(file_path: str, n: int = None) -> pd.DataFrame:
     """Load parquet file."""
     if n:
         rows = next(ParquetFile(file_path).iter_batches(batch_size=n))
@@ -20,12 +21,14 @@ def load_data(file_path: str, n: int) -> pd.DataFrame:
 def get_rows(df: pd.DataFrame) -> Generator[pd.Series, None, None]:
     """Get rows of a dataframe one by one."""
     for _, row in df.iterrows():
-        time.sleep(2)
+        time.sleep(1)
         yield row
 
 
-def upload_data(processed_data: pd.DataFrame) -> None:
+async def upload_data(processed_data: pd.DataFrame) -> None:
     """Upload data to the dashboard."""
     # TODO: Implement this function with real upload logic.
-    
-    print(processed_data.keys())
+
+    await asyncio.sleep(1)
+    print(processed_data)
+    # pass
