38dce657b2f6129351e6e42f1142b095f97190c1
moritzrengert <moritz@rengert.de>
2025-01-31 16:37:09 +0100

    fix type error

--- processing/processing.py
+++ processing/processing.py
@@ -7,7 +7,7 @@ import asyncio
 
 import pandas as pd
 from processing.helpers import get_rows, load_data, upload_data
-from processing.prediction import partial_fit_predict
+# from processing.prediction import partial_fit_predict
 from processing.workload_state import WorkloadState
 
 STATE_STORAGE_TIMER = 60
@@ -29,12 +29,12 @@ async def process_dataframe(
             asyncio.create_task(state.save_state())
             last_save_time = time.time()
 
-        state = partial_fit_predict(state)
+        # state = partial_fit_predict(state)
         processed_data.append(state)
 
         # allow other tasks to run
         await asyncio.sleep(0)
-    return pd.DataFrame(processed_data)
+    return pd.concat(processed_data, ignore_index=True)
 
 
 if __name__ == "__main__":
