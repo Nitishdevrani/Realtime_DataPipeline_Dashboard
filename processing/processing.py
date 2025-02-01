2c58ebc12726809ff840e3ad217c2e4e8b6bf5b6
moritzrengert <moritz@rengert.de>
2025-02-01 10:54:02 +0100

    offload producer blocking code and use data timestamp to accumulate results

--- processing/processing.py
+++ processing/processing.py
@@ -9,7 +9,7 @@ from processing.helpers import get_rows, load_data, upload_data
 from processing.workload_state import WorkloadState
 from processing.prediction import RealTimePredictor
 
-STATE_STORAGE_TIMER = 60
+STATE_STORAGE_TIMER = 10
 rt_predictor = RealTimePredictor(window_size=200, step_interval=100)
 
 
@@ -17,7 +17,7 @@ async def process_dataframe(
     df: pd.DataFrame, state: WorkloadState
 ) -> pd.DataFrame:
     """Process a DataFrame in a more efficient, batched fashion."""
-    last_save_time = time.time()
+    # last_save_time = time.time()
     processed_states = []
 
     # itertuples usually faster than iterrows
@@ -26,9 +26,32 @@ async def process_dataframe(
         updated_state = await asyncio.to_thread(state.update_state, row_dict)
 
         # Save state asynchronously.
-        if time.time() - last_save_time >= STATE_STORAGE_TIMER:
-            asyncio.create_task(state.save_state())
-            last_save_time = time.time()
+        # print(time.time() - last_save_time)
+        # if time.time() - last_save_time >= STATE_STORAGE_TIMER:
+        #     print("start saving state")
+        #     asyncio.create_task(state.save_state())
+        #     last_save_time = time.time()
+        overall_ts = updated_state["overall"]["timestamp"]
+        if overall_ts is not None:
+            # convert to a Python datetime if it's a pandas Timestamp
+            overall_dt = (
+                overall_ts.to_pydatetime()
+                if isinstance(overall_ts, pd.Timestamp)
+                else overall_ts
+            )
+            print(f"overall_dt: {overall_dt}")
+            # if this is the first valid timestamp
+            if state.last_backup_timestamp is None:
+                print("initializing last_backup_timestamp")
+                state.last_backup_timestamp = overall_dt
+            print(f"last_backup_timestamp: {state.last_backup_timestamp}")
+            # Calculate the elapsed time in seconds.
+            elapsed = (overall_dt - state.last_backup_timestamp).total_seconds()
+            print(f"elapsed: {elapsed}")
+            if elapsed >= STATE_STORAGE_TIMER:
+                print(f"start saving state at: {overall_ts}")
+                asyncio.create_task(state.save_state())
+                state.last_backup_timestamp = overall_ts
 
         # If the predictor is running on CPU
         updated_state = await asyncio.to_thread(
